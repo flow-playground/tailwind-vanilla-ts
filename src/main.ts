896b44abe694e75fb39518686601218c0472b9d0
pensuke <pensuke81@gmail.com>
2022-05-05 11:07:33 +0900

    Install tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,5 @@ import "./style.css";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = /* HTML */ `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank"
-    >Documentation</a
-  >
+  <h1 class="text-indigo-500">Hello Vite x tailwindcss</h1>
 `;
